140ded03f19d9e5c949addce28245f5340686466
Hashan sathsara <officialhs95@gmail.com>
2024-09-06 17:57:12 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=2c1FnZDR#hd0lgujySGmOKcDyUC97tRQ3E7TJcxydY9mrWlA-k1Q' : process.env.SESSION_ID,
-OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94713130673' : process.env.OWNER_NUMBER,   
+OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94713520744' : process.env.OWNER_NUMBER,   
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
 ONLY_ME: process.env.ONLY_ME === undefined ? 'false' : process.env.ONLY_ME,
 AUTO_STATUS_READ:  process.env.AUTO_STATUS_READ  || false  ,    
